68b9117fe6716823a7f7fa57e16880ff9fa4e039
Aysser jbili <aysser21@gmail.com>
2019-04-06 21:54:12 +0100

    update new sim

---
+++ js/zimcode/circuit.js
@@ -0,0 +1,86 @@
+ var scaling = "fit";
+
+ var assets = ["battrey.png", "pil.png", "ll1.png", "lo1.png"];
+ var path = "assets/images/circuit/";
+ var width = 1024;
+ var height = 768;
+
+
+ var frame = new Frame(scaling, width, 768, null, null, assets, path);
+ frame.on("ready", function() {
+
+
+   var stage = frame.stage;
+   var stageW = frame.width;
+   var stageH = frame.height;
+   frame.outerColor = "#444";
+   frame.color = "#99c1ff";
+
+   battry = frame.asset('battrey.png').addTo(stage).pos(100, 100).sca(0.5);
+   pil = frame.asset("pil.png").addTo(stage).pos(100, 400).sca(0.6);
+   lamp_light= frame.asset("lo1.png").pos(400, 200).sca(0.3);
+   lamp = frame.asset("ll1.png").pos(100, 400).sca(0.3);
+   var current;
+
+ 	lamp_light.on("mousedown", function(e) {
+ 		current = e.target;
+ 		current.copy = current.clone()
+
+ 			.addTo()
+ 			.drag();
+
+ 		// 4. put the copy under the object we are dragging
+ 		stage.setChildIndex(current, stage.numChildren-1);
+ 		current.startX = copy.x = current.x;
+ 		current.startY = copy.y = current.y;
+ 		stage.update();
+ 	});
+
+
+ //	5. on a pressup, swap the two objects
+ 	lamp_light.on("pressup", function(e) {
+ 		current = e.currentTarget;
+ 		// swap positions
+
+ 		swapProperties("x", current, current.copy);
+ 		swapProperties("y", current, current.copy);
+   //
+ 	// 	// or do the above manually:
+ 	// 	//current.copy.x = current.x;
+ 	// 	//current.copy.y = current.y;
+ 	// 	//current.x = current.startX;
+ 		//current.y = current.startY;
+
+ 		stage.update();
+ 	});
+
+
+
+
+   bbb = new List({
+     width: 200,
+     height: stageH,
+     list: [
+       battry, pil, lamp,lamp_light
+     ],
+     borderWidth: 4,
+     borderColor:"black",
+     backdropColor:"#ffffff",
+     close: false,
+     scrollBarColor:"red",
+     currentSelected:true,
+     swipe:false,
+     vertical: true,
+     spacing: 0,
+     align: "center",
+   }).addTo();
+
+   // 1. make object
+
+
+	// 3. on mousedown copy and set copy to drag
+
+
+   stage.update();
+
+ }); // end of ready
--- js/zimcode/plante_4.js
+++ js/zimcode/plante_4.js
@@ -1,4 +1,4 @@
-var scaling = "fit";
+ var scaling = "fit";
 
 var assets = ["table.jpg", "plante.png"];
 var path = "assets/images/plante/";
@@ -13,14 +13,17 @@ frame.on("ready", function() {
   var stage = frame.stage;
   var stageW = frame.width;
   var stageH = frame.height;
-   frame.outerColor = "#444";
-   frame.color = frame.white;
+  frame.outerColor = "#444";
+  frame.color = frame.white;
 
   // container item one tube +support cadre
   table = frame.asset("table.jpg").sca(.5).centerReg(stage);
   table.pos(0, 400);
-  panel = new Panel({height:stageH,titleBar:"      أدوات التجربة"}).addTo(stage)
-  panel.pos(stageW-panel.width,0)
+  panel = new Panel({
+    height: stageH,
+    titleBar: "      أدوات التجربة"
+  }).addTo(stage)
+  panel.pos(stageW - panel.width, 0)
 
   plante = frame.asset("plante.png").sca(.2).centerReg(panel).drag({
     onTop: false
@@ -45,19 +48,20 @@ frame.on("ready", function() {
   t4.graphics.f().s("rgba(0,0,0,0.6)").ss(2.9, 1, 1).p("Ak1jMQEOBYFdhYIAAGZIprAAg");
   t4.setTransform(119, 43.5);
 
-  t5 = new Shape();
+  t5 = new Shape
+
   t5.graphics.f("rgba(51,51,51,0.498)").s().p("Ak1DNIAAmZQEOBYFdhYIAAGZg");
   t5.setTransform(119, 43.5);
   // stopper--
 
-  t6 = new Rectangle(54, 29).addTo(stage).drag({
-    onTop: false
-  });
-  t7 = new Rectangle(27, 29, 'blue').addTo(t6);
-  t6.y = 250;
-  t6.x = 680
-  t6.regX = t6.width / 2;
-  t6.regY = t6.height;
+  // t6 = new Rectangle(54, 29).addTo(stage).drag({
+  //   onTop: false
+  // });
+  // t7 = new Rectangle(27, 29, 'blue').addTo(t6);
+  // t6.y = 250;
+  // t6.x = 680
+  // t6.regX = t6.width / 2;
+  // t6.regY = t6.height;
 
   // الماء متحرك -- Water animated--
 
@@ -76,13 +80,15 @@ frame.on("ready", function() {
   eau.scaleY = 1;
 
 
-  plante.on("mousedown", function() {
-    tf.carry = false;
-  });
+  tmp1 = false;
+  tmp2 = false;
+  tmp3 = false;
 
   plante.on("pressup", function() {
     // 3. inside the function do a conditional for the hitTest
     if (plante.hitTestBounds(tf)) {
+      tmp1 = true;
+
       plante.animate({
         props: {
           x: tf.x,
@@ -93,18 +99,20 @@ frame.on("ready", function() {
         set: {
           percentSpeed: 1
         }
+
       })
-      var resultat = true;
+      plante.noDrag()
+
     }
     stage.update();
   });
   tf.on("pressup", function() {
 
     if (tf.hitTestBounds(table)) {
-
+      tmp2 = true;
       tf.animate({
         props: {
-          x: -(table.x+150),
+          x: -(table.x + 150),
           y: table.y - tf.height
         },
         ease: "linear",
@@ -113,28 +121,30 @@ frame.on("ready", function() {
           percentSpeed: 1
         }
       })
+      tf.noDrag()
 
     }
     stage.update();
   });
-  t6.on("pressup", function() {
-
-    if (t6.hitTestBounds(tf)) {
-      t6.animate({
-        props: {
-          x: tf.x + 1.9,
-          y: tf.y - 72
-        },
-        ease: "linear",
-        time: 400,
-        set: {
-          percentSpeed: 1
-        }
-      })
-
-    }
-    stage.update();
-  });
+  // t6.on("pressup", function() {
+  //
+  //   if (t6.hitTestBounds(tf)) {
+  //     tmp3 = true;
+  //     t6.animate({
+  //       props: {
+  //         x: tf.x + 1.9,
+  //         y: tf.y - 72
+  //       },
+  //       ease: "linear",
+  //       time: 400,
+  //       set: {
+  //         percentSpeed: 1
+  //       }
+  //     })
+  //
+  //   }
+  //   stage.update();
+  // });
 
 
 
@@ -146,118 +156,53 @@ frame.on("ready", function() {
     currentTarget: true
   });
   tf.pos(20, 250)
+  if (plante.hitTestBounds(tf) == false || tf.hitTestBounds(table) == false) {
+    tmp1 = tmp2 = false;
 
-  // slider = new Slider(1, 10).pos(650, 10);
-  //
-  // slider.on("change", function() {
-  //   eau.percentSpeed = Math.floor(slider.currentValue * 10)
-  //   label.text = "تسريع :" + Math.floor(slider.currentValue)
-  //
-  // });
-  //
-  // var label = new Label({
-  //   text: "تسريع : " + 1,
-  //   size: 20,
-  //   font: "courier",
-  //   color: "black",
-  //   rollColor: "red",
-  //   fontOptions: "italic bold",
-  // });
-  // label.pos(650, 50);
-
-
-
-  // tf.on("pressup", function(e) {
-  //
-  //
-  //
-  //     if (tf.hitTestReg(table)  ) {
-  //       zog("hhhh")
-  //
-  //
-  //
-  //       var point = table.localToLocal(table.x, table.y, tf);
-  //       tf.animate({x:point.x, y:point.y}, 100);
-  //
-  //       result = "done"
-  //     } else {
-  //       zog("bb")
-  //
-  //       result=""
-  //     }
-  //
-  //   if (result == "done") return;
-  //   zog("3")
-  //
-  //   // 9. if the object hits no targets then move the target back to the startX and startY
-  //   tf.animate({x:tf.startX, y:tf.startY}, 300);
-  // });
-
-  // 10. optional highlight the target if they are eligable for drop - use a pressmove event
-
-
-  // new list item
-  // if add sup to list show the message §§
-  // console.error() ===>width needs bounds set with setBounds()  "Cannot read property 'width' of undefined" ;
-  // var list = new List({
-  //   list: [sup],
-  //   titleBar: "items",
-  //
-  //   viewNum: 3
-  //
-  // }).center().pos(-1, 0)
-
-
-  // fn drag item list
-  // var dg = function drag_it(obj_item, current) {
-  //
-  //   obj_item.on("mousedown", function(e) {
-  //     current = e.target;
-  //     current.copy = current.clone()
-  //       .addTo()
-  //       .pos(200.200)
-  //       .transform()
-  //       .drag()
-  //
-  //
-  //     stage.setChildIndex(current, stage.numChildren - 1);
-  //     current.startX = copy.x = current.x;
-  //     current.startY = copy.y = current.y;
-  //     stage.update()
-  //
-  //   });
-  //
-  //
-  //   obj_item.on("pressup", function(e) {
-  //     current = e.currentTarget;
-  //
-  //     stage.update();
-  //   });
-  //
-  //
-  //
-  // }
-  //dg(sup); // call fn drag(item)
-  // reeee
-
-
-
-  // resize
-  frame.on("resize", resize);
-
-  function resize(e) {
-    stageW = frame.width;
-    stageH = frame.height;
-
-
-
-    stage.update();
   }
-  resize();
 
+  Ticker.add(function() {
 
+    if (tmp1 & tmp2) {
+      eau.animate({
+        props: {
+          scaleY: 0.5
+        },
 
+        ease: "linear",
+        time: 40000,
+        set: {
+          percentSpeed: 1
+        }
+      })
+    }
 
-  stage.update();
+  });
+
+  var label = new Label({
+   text:"توقف",
+   size:40,
+   backgroundColor:"violet", //
+   fontOptions:"bold"
+});
+var btn = new Button({
+   label:label,
+   width:250,
+   height:90,
+   toggle:'جرب الان',
+   backgroundColor:"purple",
+   rollBackgroundColor:"MediumOrchid",
+   borderWidth:2,
+   borderColor:"violet",
+   gradient:.3,
+   corner:1
+});
+btn.addTo(stage);
+btn.pos(stageW-(panel.width+btn.width),0)
+btn.on("mousedown", function() {
+   eau.pauseAnimate(!eau.paused);
+});
+
+   stage.update();
 
 }); // end of ready
