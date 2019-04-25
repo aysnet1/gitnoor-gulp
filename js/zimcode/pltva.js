dc5301fda5f24b4ffaf7a5d35cf1c68c3cc517e8
Aysser jbili <aysser21@gmail.com>
2019-04-25 12:07:08 -0700

    add new sim

--- js/zimcode/pltva.js
+++ js/zimcode/pltva.js
@@ -1,8 +1,7 @@
   var scaling = "fit";
 
-var assets = ["tab.png","pltmort.json","pltmort.png","ter1.png","ter.png","pltmort1.json","pltmort1.png",
-"put.png","putv.png","pltviv.png" ,"racin.png","ra.png",
-"cup1.png","cup2.png","mira.png","pwat.json","pwat.png"];
+var assets = ["tab.png","plteau.png","pltsa.png","ter1.png","ter.png","eva.png","peva.png","sac.json","sac.png","sac1.json","sac1.png",
+"put.png","putv.png","mira.png","pwat.json","pwat.png"];
 var path = "assets/images/plante/";
 var width = 1024;
 var height = 768;
@@ -73,11 +72,6 @@ qst = new Label({text:"التّعليمة: قم بوضع كل نبتة في أص
 
 
 
-     panel = new Panel({
-       height: stageH/2+200 ,
-       titleBar: "          النبتة"
-     }).addTo(stage)
-     panel.pos(stageW - panel.width, 0)
 boundary = new Boundary(0,0,stageW,stageH).contract(90)
 bplt1 = new Boundary(0,200,stageW/2,stageH-200)
 bplt2 = new Boundary(stageW/2,200,stageW/2,stageH-250)
@@ -95,16 +89,12 @@ plt.y=640.0033388981636
 plt2.x=512
 plt2.y=500.2938230383972
 
-plante = new Sprite({json:frame.asset("pltmort.json")})
-p2 = new Sprite({json:frame.asset("pltmort1.json")}).centerReg(plt2)
+plante = frame.asset("pltsa.png").sca(0.45)
+p2 = frame.asset("plteau.png").centerReg(plt2).sca(0.45)
 plante.centerReg(plt);
 plt.drag({currentTarget:true,onTop:false,boundary:bplt1})
 plt2.drag({currentTarget:true,onTop:false,boundary:bplt2})
-racin = frame.asset("racin.png")
-ra =  frame.asset("ra.png").centerReg(plt2).mov(10,26).sca(0.4)
-ra.alpha=0
-racin.centerReg(plt).mov(10,26)
-racin.sca(0.4)
+
 ter = frame.asset("ter.png").centerReg().sca(0.09)
 ter1 = frame.asset("ter1.png").centerReg().sca(0.09)
 ter1.x=70.49708284714119
@@ -119,8 +109,21 @@ br = new Rectangle(50,50,'transparent').centerReg(plt2).mov(28,-68)
 
 putm=new Container().setBounds(0,0,300,400).centerReg(stage).mov({x:-250,y:250}).sca(0.35)
  putv=new Container().setBounds(0,0,300,400).centerReg(stage).mov({x:200,y:250}).sca(0.35)
-     a1=  frame.asset("putv.png").centerReg(putm)
-     a2=  frame.asset("put.png").centerReg(putv)
+     a1=  frame.asset("putv.png").centerReg(putm);
+     a2=  frame.asset("put.png").centerReg(putv);
+
+   vap = frame.asset("eva.png").centerReg(plt2).sca(0.4)
+   vap.y=-5
+  vap.alpha=0.001
+   vap1 = frame.asset("peva.png").centerReg(plt).sca(0.4)
+   vap1.y=-10
+   vap1.alpha=0.001
+
+
+    sw = new Sprite({json:frame.asset("sac.json")}).centerReg().sca(0.5).mov(160,-190);
+    sw1= new Sprite({json:frame.asset("sac1.json")}).centerReg().sca(0.5).mov(-200,-200);
+
+
 
 
    mira = new Container().setBounds(0,0,150,214).centerReg(stage)
@@ -132,12 +135,27 @@ pw =  new Sprite({json:frame.asset("pwat.json")}).sca(0.5).centerReg(mira).mov(-
    mira.x=921.8389731621937
    mira.y=650.4550758459743
 
+
+
+
+
+
+
+
 pw.visible=false
+
+
+swX = 705.5861271676301
+swY=386.3676300578035
+sw1X =259.8473988439306
+sw1Y=386.43236994219643
+
+
 mira.on("pressup",()=>{
- if ((plt.x==putm.x-15)  && (plt2.x==putv.x-15) && (ter.x==putv.x-20) && (ter1.x==putm.x-20)) {
+ if ((plt.x==putm.x-20)  && (plt2.x==putv.x-10) && (ter.x==putv.x-20) && (ter1.x==putm.x-20) )  {
 
    Ticker.add(()=>{
-  if (mira.hitTestRect(brt) || mira.hitTestRect(br)){
+ if (mira.hitTestRect(brt) || mira.hitTestRect(br)){
 
 mira.animate({
    props:{rotation:-45},
@@ -161,15 +179,24 @@ else{
 
 
 }
-if (mira.hitTestRect(plt2)){
-   test=true
-}
-if (mira.hitTestRect(plt)) {
-  test1=true
-}
 
-if (test==test1) {
-  startb.visible=true
+
+if ((sw.x==swX) && (sw1.x==sw1X)) {
+  vap.animate({
+    props:{
+      alpha:0.9
+    },
+    time:2000
+
+
+  });
+  vap1.animate({
+    props:{
+      alpha:0.9
+    },
+    time:1500
+ });
+
  }
 
 })
@@ -184,7 +211,7 @@ function lotme(a,b,c,x,y,t) {
 
 
    this.prez = a.on("pressmove",function() {
-  if (a==plt2){
+  if ((a==plt2) || (a == ter)) {
    if (a.hitTestBounds(b) ) {
         a.noDrag()
        a.animate({
@@ -215,7 +242,7 @@ function lotme(a,b,c,x,y,t) {
           events:true
 
     })
-    if ((c == ter) || (c == ter1)){
+    if ((c == sw1) || (c == ter1)){
      c.drag({onTop:false,boundary:bplt1})
    }
 
@@ -231,19 +258,56 @@ function lotme(a,b,c,x,y,t) {
   })
 }
 
-bb2   =   new lotme(plt,putm,ter1,putm.x-15,putm.y-100,400)
-bb3   =   new lotme(plt2,putv,ter,putv.x-15,putv.y-100,400)
-tera1 =   new lotme(ter,putv,p2,putv.x-20,putv.y-48,400)
-tera2 =   new lotme(ter1,putm,p2,putm.x-20,putm.y-48,400)
 
 
 
-startb.on("click", function() {
-p2.run({time:30000})
-startb.animate({props:{alpha:0},time:5000})
- });
+function rimi(a,b,x,y,t,ts,label) {
 
-btn1.on("click", function() {zgo("/pltmort.html");});
+
+   this.prez = a.on("pressmove",function() {
+
+
+
+  if (a.hitTestRect(b) ) {
+      a.noDrag()
+      a.sca(1.2,1)
+     a.animate({
+          props:{
+            x:x,
+            y:y
+          },
+
+          time:t,
+          events:true
+
+    })
+     a.run(ts,label)
+   }
+
+
+  })
+
+  this.cap = a.on("animation",()=>{
+    a.off("pressmove",this.prez)
+   })
+}
+// position sac
+
+
+
+bb2   =   new lotme(plt,putm,ter1,putm.x-20,putm.y-160,400)
+bb3   =   new lotme(plt2,putv,ter,putv.x-10,putv.y-160,400)
+tera1 =   new lotme(ter,putv,sw,putv.x-20,putv.y-48,400)
+tera2 =   new lotme(ter1,putm,sw1,putm.x-20,putm.y-48,400)
+                 //(a,b,x,y,t,ts,label)
+                 //test sac
+sacer =   new rimi(sw,vap,swX,swY,400,4000,"close")
+sacer1 =   new rimi(sw1,vap1,sw1X,sw1Y,400,4000,"close")
+
+
+
+
+btn1.on("click", function() {zgo("/pltva.html");});
 
 
 
